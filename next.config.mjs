3409d913c9faf0accf223d00c65466c390f3d4de
Filip Kalný <filip@kalny.net>
2024-05-16 22:43:41 +0200

    put skills in table

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,19 @@
-import createMDX from '@next/mdx';
+import createMDX from "@next/mdx";
+import remarkGfm from "remark-gfm";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
-  pageExtensions: ['md', 'mdx', 'ts', 'tsx'],
+  pageExtensions: ["md", "mdx", "ts", "tsx"],
 };
- 
+
 const withMDX = createMDX({
   extension: /\.mdx?$/,
+  options: {
+    remarkPlugins: [remarkGfm],
+    rehypePlugins: [],
+  },
 });
- 
+
 // Merge MDX config with Next.js config
 export default withMDX(nextConfig);
