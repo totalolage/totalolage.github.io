9a880de74a839f2a96854ae19bc4b56bfa5bf433
Filip Kalný <filip@kalny.net>
2026-06-05 13:07:35 +0200

    chore: Update eslint config typing

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,7 @@
 import eslintPluginAstro from 'eslint-plugin-astro';
 import eslintPluginPrettier from 'eslint-plugin-prettier';
 
-/** @type {import("eslint").Linter.FlatConfig[]} */
+/** @type {import("eslint").Linter.Config[]} */
 export default [
   ...eslintPluginAstro.configs.recommended,
   ...eslintPluginPrettier.configs.recommended,
